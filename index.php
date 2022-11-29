108cf980187353953887b4967ae6645d9b0fa0f4
evgrbk <66783664+evgrbk@users.noreply.github.com>
2022-11-29 13:26:48 +0300

    fix

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+namespace App;
+
+
+require_once __DIR__.'/vendor/autoload.php';
+
+class Foo {
+    public $a = "foobarstring";
+    public $b;
+    public $c = ['some', 'values'];
+	
+	public function func(){
+		echo 'sdfsdf '.__CLASS__;
+		/*for ($i=0; $i<1000; $i++) {
+			$m = memory_get_usage();
+			${'var'.$i} = new self;
+			echo memory_get_usage() - $m"\n";
+		}*/
+	}
+	
+}
+
+//$a = new Foo();
+//$a->func();
--- src/Foo.php
+++ src/Foo.php
@@ -1,21 +1,21 @@
 <?php
-namespace Rbkevg\Codescan;
-
-
-require __DIR__.'/vendor/autoload.php';
+namespace Rbkevg\Codescan\;
 
 class Foo {
     public $a = "foobarstring";
     public $b;
     public $c = ['some', 'values'];
 	
-	public func(){
-		for ($i=0; $i<1000; $i++) {
+	public function func(){
+		echo 'sdfsdf '.__CLASS__;
+		/*for ($i=0; $i<1000; $i++) {
 			$m = memory_get_usage();
 			${'var'.$i} = new self;
 			echo memory_get_usage() - $m"\n";
-		}
+		}*/
 	}
 	
 }
 
+//$a = new Foo();
+//$a->func();
